1e3974383e26dc14aaf79f40fc4547e2f82d179b
Moises <Moises@192.168.15.3>
2020-10-23 01:36:59 -0500

    23-10-2020 -LongestCommonPrefix -Divide2Numbers

--- src/leetcode/Divide2Numbers.java
+++ src/leetcode/Divide2Numbers.java
@@ -3,6 +3,26 @@ package leetcode;
 public class Divide2Numbers {
 
 	public int divide(int dividend, int divisor) {
-		return 0;
+		int count = 0;
+		if(Integer.MIN_VALUE == dividend && divisor == -1) return Integer.MAX_VALUE;
+		if(dividend == divisor) return 1;
+		if(divisor == 0 || divisor == Integer.MIN_VALUE) return 0;
+		
+		if(dividend == Integer.MIN_VALUE){
+			count++;
+            dividend += Math.abs(divisor);
+        }
+		
+		int absDividend = Math.abs(dividend);
+		int absDivisor = Math.abs(divisor);
+		boolean flag = (dividend<0 && divisor > 0 || dividend > 0 && divisor < 0)?true:false;
+		while(absDivisor <= absDividend) {
+			absDividend -=absDivisor;
+			count++;
+		}
+		if(flag) {
+			count=-count;
+		}
+		return count;
 	}
 }
--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -9,29 +9,31 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/*
 		//456
 		ListNode node1 = new ListNode(5, new ListNode(6, new ListNode(4)));
-		
 		//321
 		ListNode node6 = new ListNode(3, new ListNode(2, new ListNode(1)));
-		
 		SolutionAddTwoNumbers sn = new SolutionAddTwoNumbers();
 		ListNode res = sn.addTwoNumbers(node1, node6);
-		
 		while(res != null) {
 			System.out.println(res.val);
 			res = res.next;
 		}
+		*/
 		
+		/*
 		//Most frequent elements on an array
 		MostFrequentKElements mostFE = new MostFrequentKElements();
 		String[] array = {"hola", "adios", "hola", "hola", "saludos", "adios","otro"};
 		int k = 3;
 		//deberia de imprimir hola y adios
-		//System.out.println(mostFE.mostFElements(array, k));
-		//System.out.println("-----con lambda-------");
-		//System.out.println(mostFE.mostFElementsLamda(array, k));
+		System.out.println(mostFE.mostFElements(array, k));
+		System.out.println("-----con lambda-------");
+		System.out.println(mostFE.mostFElementsLamda(array, k));
+		*/
 		
+		/*
 		// sum 3 numbers
 		Sum3numbers s3 = new Sum3numbers();
 		int[] nums = {-1,0,1,2,-1,-4};
@@ -39,17 +41,26 @@ public class Main {
 		quickSort.sort(nums);
 		System.out.println(Arrays.toString(nums));
 		//System.out.println(s3.threeSum(nums));
+		*/
 		
+		
+		//longest common prefix
 		String[] words = {"ab","a"};
 		LongestCommonPrefix lcp = new LongestCommonPrefix();
 		System.out.println(lcp.longestCommonPrefix(words));
 		
+		/*
 		//Sum 3 closes
 		int target = 1;
 		int[] numArr = {-1,2,1,-4};
 		Sum3Closest sum3Closest = new Sum3Closest();
 		System.out.println(sum3Closest.threeSumClosest(numArr, target));
+		*/
 		
-		
+		/*
+		Divide2Numbers d2n = new Divide2Numbers();
+		int res = d2n.divide(-2147483648, 2);
+		System.out.println(res);
+		*/
 	}
 }
